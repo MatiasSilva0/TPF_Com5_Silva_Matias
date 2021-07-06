afa8d1a9888b96afc7ff6fa112f505efedaab449
MatiasSilva0 <matis7390@gmail.com>
2021-07-06 15:22:28 -0300

    V1.0

---
+++ TPF_Com5_Silva_Matias/juego/test/Estrategia.cs
@@ -0,0 +1,198 @@
+﻿
+using System;
+using System.Collections.Generic;
+namespace DeepSpace
+{
+
+	class Estrategia
+	{
+		
+        public String Consulta1( ArbolGeneral<Planeta> arbol)
+		{
+			Cola<ArbolGeneral<Planeta>> c = new Cola<ArbolGeneral<Planeta>>();
+			ArbolGeneral<Planeta> aux = null;
+			bool check = false;
+			c.encolar(arbol);
+			
+			// busco raíz de la IA
+			// si check = true, encontré la raíz
+			
+			while(!check)
+			{
+				aux = c.desencolar();
+				if(aux.getDatoRaiz().EsPlanetaDeLaIA())
+					check = true;
+				else
+				{
+					foreach(ArbolGeneral<Planeta> hijo in aux.getHijos())
+						c.encolar(hijo);
+				}
+			}
+			
+			// le busco la altura a la raíz
+			return "La distancia entre la raíz y la hoja más lejana es: "+aux.altura();
+		}
+
+		public String Consulta2( ArbolGeneral<Planeta> arbol)
+		{
+			Cola<ArbolGeneral<Planeta>> c = new Cola<ArbolGeneral<Planeta>>();
+			ArbolGeneral<Planeta> aux;
+			int x = 0; // contador
+			c.encolar(arbol);
+			
+			// mientras cola no se vacíe
+			while(!c.esVacia())
+			{
+				aux = c.desencolar();
+				foreach(ArbolGeneral<Planeta> hijo in aux.getHijos())
+					c.encolar(hijo);
+				
+				// si población > 3, incremento contador
+				if(aux.esHoja() && aux.getDatoRaiz().population > 3)
+					x = x + 1;
+			}
+			return ("El número de planetas con población mayor a 3 es: "+x);
+		}
+		
+		public String Consulta3( ArbolGeneral<Planeta> arbol)
+		{
+			Cola<ArbolGeneral<Planeta>> c = new Cola<ArbolGeneral<Planeta>>();
+			ArbolGeneral<Planeta> aux;
+			c.encolar(arbol);
+			c.encolar(null);
+			uint pro = promedio(arbol);
+			uint count = 0;
+			uint nivel = 0;
+			string mensaje = "Número de planetas con población mayor al promedio:\n";
+			
+			// mientras cola no se vacíe
+			while(!c.esVacia())
+			{
+				aux = c.desencolar();
+				
+				// si no terminé con el nivel
+				if(aux != null)
+				{
+					// si población > promedio, incremento contador
+					if(aux.getDatoRaiz().population > pro)
+						count = count + 1;
+					foreach(ArbolGeneral<Planeta> hijo in aux.getHijos())
+						c.encolar(hijo);
+				}
+				
+				// si ya terminé con el nivel
+				else
+				{
+					// imprimo mensaje del nivel
+					mensaje = mensaje + ("Nivel "+nivel+": "+count+"\n");
+					
+					// reinicio contador y paso al sig. nivel
+					nivel = nivel + 1;
+					count = 0;
+					
+					// si la cola está vacía, terminé de explorar el árbol 
+					if(!c.esVacia())
+						c.encolar(null);
+				}
+			}
+			return mensaje;
+		}
+		
+		public uint promedio(ArbolGeneral<Planeta> arbol)
+		{
+			Cola<ArbolGeneral<Planeta>> c = new Cola<ArbolGeneral<Planeta>>();
+			ArbolGeneral<Planeta> aux;
+			c.encolar(arbol);
+			uint pro = 0;
+			uint count = 0;
+			
+			// mientras cola no se vacíe
+			while(!c.esVacia())
+			{
+				aux = c.desencolar();
+				
+				// sumo al total la población del planeta e incremento contador
+				pro = pro + aux.getDatoRaiz().population;
+				count = count + 1;
+				foreach(ArbolGeneral<Planeta> hijo in aux.getHijos())
+					c.encolar(hijo);
+			}
+			return (pro / count);
+		}
+		
+		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
+		{
+			// si la raíz no es del IA
+			if(!arbol.getDatoRaiz().EsPlanetaDeLaIA())
+			{
+				Pila<ArbolGeneral<Planeta>> pila = new Pila<ArbolGeneral<Planeta>>();
+				pila.apilar(arbol);
+				Pila<ArbolGeneral<Planeta>> p = capturarRaiz(pila);
+				
+				Planeta origen = p.tope().getDatoRaiz();
+				Planeta destino = p.destino().getDatoRaiz();
+				Movimiento mov = new Movimiento(origen, destino);
+				return mov;
+			}
+			
+			else
+			{
+				Pila<ArbolGeneral<Planeta>> pila = new Pila<ArbolGeneral<Planeta>>();
+				pila.apilar(arbol);
+				Pila<ArbolGeneral<Planeta>> p = capturarHijo(pila);
+				
+				// busco origen
+				ArbolGeneral<Planeta> origen = null;
+				while(!p.tope().getDatoRaiz().EsPlanetaDeLaIA())
+					origen = p.desapilar();
+				
+				Movimiento mov = new Movimiento(p.tope().getDatoRaiz(), origen.getDatoRaiz());
+				return mov;
+			}
+		}
+		
+		public Pila<ArbolGeneral<Planeta>> capturarRaiz(Pila<ArbolGeneral<Planeta>> pila)
+		{
+			List<ArbolGeneral<Planeta>> hijos = pila.tope().getHijos();
+			
+			// por cada hijo
+			foreach(ArbolGeneral<Planeta> hijo in hijos)
+			{
+				// añadir al camino
+				pila.apilar(hijo);
+				
+				// si es planeta de la IA, retornar camino
+				if(pila.tope().getDatoRaiz().EsPlanetaDeLaIA())
+					break;
+				
+				capturarRaiz(pila);
+				if(pila.tope().getDatoRaiz().EsPlanetaDeLaIA())
+					break;
+				pila.desapilar();
+			}
+			return pila;
+		}
+		
+		public Pila<ArbolGeneral<Planeta>> capturarHijo(Pila<ArbolGeneral<Planeta>> pila)
+		{
+			List<ArbolGeneral<Planeta>> hijos = pila.tope().getHijos();
+			
+			// por cada hijo
+			foreach(ArbolGeneral<Planeta> hijo in hijos)
+			{
+				// añadir al camino
+				pila.apilar(hijo);
+				
+				// si es planeta del jugador, retornar camino
+				if(!pila.tope().getDatoRaiz().EsPlanetaDeLaIA())
+					break;
+				
+				capturarHijo(pila);
+				if(!pila.tope().getDatoRaiz().EsPlanetaDeLaIA())
+					break;
+				pila.desapilar();
+			}
+			return pila;
+		}
+	}
+}
---
+++ TPF_Com5_Silva_Matias/juego/test/Pila.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace DeepSpace
+{
+
+	public class Pila<T>
+	{		
+		private List<T> datos = new List<T>();
+	
+		public void apilar(T elem) {
+			this.datos.Add(elem);
+		}
+	
+		public T desapilar() {
+			T temp = this.datos[this.datos.Count - 1];
+			this.datos.RemoveAt(this.datos.Count - 1);
+			return temp;
+		}
+		
+		public T tope() {
+			return this.datos[this.datos.Count - 1]; 
+		}
+		
+		public bool esVacia() {
+			return this.datos.Count == 0;
+		}
+		
+		public T destino() {
+			return this.datos[this.datos.Count - 2];
+		}
+	}
+}
